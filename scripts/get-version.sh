7947f36a023dcd15a2900c325f0ccd9eb01909ca
Maksim Shengeliia <maksim.shengelia@mail.ru>
2024-06-15 04:20:31 +0300

    Some config updates

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -1,5 +1,8 @@
 file="gradle.properties"
 
 cd -
-version=$(grep "rabbit.appVersion" ${file} | cut -d'=' -f2)
+version=$(grep "rabbit.android.build" ${file} | cut -d'=' -f2)
+package=$(grep "APP_PACKAGE" ${file} | cut -d'=' -f2)
+
 echo "AppVersion=${version}" >> "$GITHUB_OUTPUT"
+echo "AppPackage=${package}" >> "$GITHUB_OUTPUT"
